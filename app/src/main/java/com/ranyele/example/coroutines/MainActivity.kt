e3b50a845a43c3d9dd8d1696b81cc7eccd734bce
Ranyele Amorim <>
2021-06-04 23:42:44 -0300

    coroutine scope

--- app/src/main/java/com/ranyele/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/ranyele/example/coroutines/MainActivity.kt
@@ -8,4 +8,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Sequentially executes doWorld followed by "Done"
+    fun main() = runBlocking {
+        doWorld()
+        println("Done")
+    }
+
+    // Concurrently executes both sections
+    suspend fun doWorld() = coroutineScope { // this: CoroutineScope
+        launch {
+            delay(2000L)
+            println("World 2")
+        }
+        launch {
+            delay(1000L)
+            println("World 1")
+        }
+        println("Hello")
+    }g
 }
